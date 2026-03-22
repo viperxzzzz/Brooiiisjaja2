de9283f8ac393cd6c968c0a17e14fbd43c35c402
viperxzzzz <yeahbroitsdc@gmail.com>
2026-03-22 16:23:10 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -220,7 +220,9 @@ class GenDropdown(discord.ui.Select):
                     f"Categoria: **{categoria.upper()}**"
                 )
 
-        remove_credits(user.id, price)
+        if price > 0:
+            remove_credits(user.id, price)
+
         user_cooldowns[user.id] = time.time()
 
         await atualizar_painel()
